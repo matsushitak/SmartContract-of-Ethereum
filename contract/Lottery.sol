f9df3793675127658d785191223c754957d0b683
n-matsushita <matsushita@nextinnovation-inc.co.jp>
2018-07-18 23:49:29 +0900

    Mortal、CircuitBreakerパターンのライブラリをimport

--- contract/Lottery.sol
+++ contract/Lottery.sol
@@ -1,6 +1,11 @@
 pragma solidity ^0.4.24;
 
+// Mortalパターン
+import "../node_modules/openzeppelin-solidity/contracts/lifecycle/Destructible.sol";
+// CircuitBreakerパターン
+import "../node_modules/openzeppelin-solidity/contracts/lifecycle/Pausable.sol";
+
 // 抽選のコントラクト
-contract Lottery {
+contract Lottery is Destructible, Pausable {
     
 }
