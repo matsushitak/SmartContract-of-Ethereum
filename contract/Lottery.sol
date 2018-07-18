ac3216264ce27a09853fa434f3cfd370e1642702
n-matsushita <matsushita@nextinnovation-inc.co.jp>
2018-07-18 23:57:21 +0900

    抽選に必要な関数を用意

--- contract/Lottery.sol
+++ contract/Lottery.sol
@@ -8,4 +8,18 @@ import "../node_modules/openzeppelin-solidity/contracts/lifecycle/Pausable.sol";
 // 抽選のコントラクト
 contract Lottery is Destructible, Pausable {
     
+    // コンストラクタ
+    constructor() public {
+        
+    }
+    
+    // 応募する
+    function apply() public payable whenNotPaused {
+        
+    }
+    
+    // 抽選する
+    function lottery() public onlyOwner whenNotPaused {
+        
+    }
 }
