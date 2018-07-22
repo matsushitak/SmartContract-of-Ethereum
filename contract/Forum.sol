76505c1807d20485d604fd0c87b5a1128946ac97
n-matsushita <matsushita@nextinnovation-inc.co.jp>
2018-07-22 22:09:32 +0900

    Mortal、CircuitBreakerパターン

--- contract/Forum.sol
+++ contract/Forum.sol
@@ -1,6 +1,15 @@
 pragma solidity ^0.4.24;
 
+// Mortalパターン
+import "../node_modules/openzeppelin-solidity/contracts/lifecycle/Destructible.sol";
+// CircuitBreakerパターン
+import "../node_modules/openzeppelin-solidity/contracts/lifecycle/Pausable.sol";
+
 // 掲示板のコントラクト
-contract Forum {
+contract Forum is Destructible, Pausable {
     
+    // コンストラクタ
+    constructor() public {
+        
+    }
 }
